e2d5a24302c759ae9c45cfac05b5135136cd55ce
skl1017 <loan.riyanto.pro@gmail.com>
2024-01-28 14:59:35 +0100

    re

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './state/store';
 import Component from './Component';
+import {QueryClient, QueryClientProvider} from 'react-query';
+
+
+const queryClient = new QueryClient();
+
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
     <Provider store={store}>
     <div className="App">
      <p>Hello</p>
      <Component/>
     </div>
     </Provider>
+    </QueryClientProvider>
   );
 }
 
--- src/Component.js
+++ src/Component.js
@@ -1,10 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useQuery } from "react-query";
 import {increment, decrement} from './state/counter/counterSlice'
+import { useEffect } from "react";
+
+const fetchData = async (req) =>{
+    const res = await fetch(req);
+    const data = await res.json();
+    return data;
+  }
 
 function Component (){
 
     const dispatch=useDispatch();
-
     const counter = useSelector((state)=>state.counter);
 
     const handleIncrement=()=>{
@@ -13,6 +20,14 @@ function Component (){
     const handleDecrement=()=>{
         dispatch(decrement());
     }
+
+    
+      
+    const {data, error, isLoading} = useQuery('data', () => fetchData('https://jsonplaceholder.typicode.com/todos/1'))
+
+    if(isLoading) return <p>Loading...</p>
+    if(error) return <p>Error</p>
+    console.log(data)
 
     return(
         <>
